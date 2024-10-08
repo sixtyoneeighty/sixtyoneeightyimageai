08f3a96f4a5ee237281e99df3a618ad7f2021c37
Jacob Ambroz <143131645+sixtyoneeighty@users.noreply.github.com>
2024-10-08 02:45:18 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div 
       className="container mx-auto px-4 py-8" 
-      style={{ backgroundColor: '#050307', backgroundSize: 'cover', borderWidth: '1px', borderStyle: 'solid', borderColor: '#ccc' }}
+      style={{ backgroundColor: '#050307', backgroundSize: 'cover', borderWidth: '1px', borderStyle: 'solid', borderColor: '#000' }}
     >
       <ImageGenerator />
     </div>
